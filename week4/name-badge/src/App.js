41f61afe19568718a289550a7f30188f9a1502a1
Kelly <kelly.compton@bryanuniversity.edu>
2020-06-28 18:59:32 -0400

    #

---
+++ week4/name-badge/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BadgeForm from './badges/BadgeForm';
+
+function App() {
+  return (
+    <div>
+      <BadgeForm />
+    </div>
+  );
+}
+
+export default App;
